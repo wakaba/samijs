d6278b35f2fdbe5164caf92c01881d8c31579db3
Wakaba <w@suika.fam.cx>
2009-01-12 14:13:37 +0000

    License block and ref to wiki page added

--- NodeRectViewer-bookmarklet.js
+++ NodeRectViewer-bookmarklet.js
@@ -1,7 +1,18 @@
 javascript:
+
+if (!window.NodeRectViewer) window.NodeRectViewer = {};
+
+NodeRectViewer.DEBUG = false;
+
 var s = document.createElement ('script');
-s.src = "http://suika.fam.cx/www/css/noderect/NodeRectViewer.js?"
-    + Math.random ();
+s.src = "http://suika.fam.cx/www/css/noderect/NodeRectViewer.js";
 document.body.appendChild (s);
 
 void (0);
+
+/*
+
+Author: Wakaba <w@suika.fam.cx>
+License: Public Domain.
+
+*/
