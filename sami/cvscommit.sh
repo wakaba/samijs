bc6c5b39c633901cf0e2bce3ed59ce3a0dbdb9b5
Wakaba <w@suika.fam.cx>
2009-07-25 05:22:20 +0000

    ++ ChangeLog	25 Jul 2009 05:22:15 -0000 	* cvscommit.sh: Revert the previous change; --entry-author-name 	was not the default value, but the override.
    2009-07-25  Wakaba  <wakaba@suika.fam.cx>

--- sami/cvscommit.sh
+++ sami/cvscommit.sh
@@ -8,10 +8,9 @@ mkcommitfeed \
     --feed-related-url "http://suika.fam.cx/www/js/sami/doc/readme" \
     --feed-license-url "http://suika.fam.cx/www/js/sami/doc/readme#license" \
     --feed-rights "This feed is free software; you can redistribute it and/or modify it under the same terms as Perl itself." \
-    --entry-author-name "anon" \
     < .cvslog.tmp
 cvs commit -F .cvslog.tmp $1 $2 $3 $4 $5 $6 $7 $8 $9 
 rm .cvslog.tmp
 
-## $Date: 2009/07/25 05:19:20 $
+## $Date: 2009/07/25 05:22:20 $
 ## License: Public Domain
