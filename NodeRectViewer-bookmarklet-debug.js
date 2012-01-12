311ad53e8452b08e37f75eccbbf7371d95449a74
Wakaba <w@suika.fam.cx>
2012-01-12 14:11:21 +0000

    added

---
+++ NodeRectViewer-bookmarklet-debug.js
@@ -0,0 +1,19 @@
+javascript:
+
+if (!window.NodeRectViewer) window.NodeRectViewer = {};
+
+NodeRectViewer.DEBUG = true;
+
+var s = document.createElement ('script');
+s.src = "http://suika.fam.cx/www/css/noderect/NodeRectViewer.js?"
+    + Math.random ();
+document.body.appendChild (s);
+
+void (0);
+
+/*
+
+Author: Wakaba <w@suika.fam.cx>
+License: Public Domain.
+
+*/
